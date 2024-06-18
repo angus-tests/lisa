26c7caa319aa1c136a7486e5813cf83e175dfbcb
Angus <angus.goody@gmail.com>
2024-06-18 16:33:20 +0100

    add failed SVG

--- app/load_image.py
+++ app/load_image.py
@@ -14,6 +14,7 @@ status_to_image = {
     Status.DOWN: "down.svg",
     Status.DODGY: "maybe.svg",
     Status.MAINTENANCE: "maintenance.svg",
+    Status.FAILED: "failed.svg",
     Status.UNKNOWN: "unknown.svg"
 }
 
